054e508bb7743eb374262b27fe665a023315616c
jonghun03 <jh030512@gmail.com>
2019-03-16 22:04:42 +0900

    Deleted useless codes.

--- server.js
+++ server.js
@@ -5,24 +5,8 @@ const statichostdir = 'public' // Set static host directory.
 
 const express = require('express'), https = require('https'), http = require('http'), fs = require('fs'); // 각종 모듈 로드
 const urlz = require('url');
-const bodyparse = require('body-parser');
-const querystring = require('querystring');
-var requestz = require('request'),
-    cheerio = require('cheerio'),
-    cookie = require('cookie');
 var main = express();
 main.use('/static', express.static(__dirname + '/' + statichostdir)); 
-var formidable = require('formidable');
-
-function decodeBase64Image(dataString) {
-  try {
-    return new Buffer(dataString, 'base64');;
-  } catch(e) {
-    console.log(e);
-
-    return dataString;
-  }
-}
 
 function scandir() {
   if (fs.existsSync('../garrysmod')) {
@@ -43,27 +27,8 @@ main.get('/*', ( request, response ) => {
   RequestExcute( request.url, request.connection.remoteAddress , request, response );
 });
 
-
-main.post('/*', (request, response) => {
-  var form = new formidable.IncomingForm();
-  form.parse(request, function (err, fields, files) {
-    console.log("ERR: " + err);
-    console.log(fields);
-    
-    try{
-      fs.writeFile("./uploadedImage/" + fields.nick + "_" + fields.pl + "_" + Math.floor(Math.random() * 100000) + "_up.jpg", decodeBase64Image(fields.data), () => {
-        response.write('Server net message');
-        response.end();
-      });
-    } catch(e) {
-      console.log(e)
-    }
-    
-  });
-});
-
 function RequestExcute( url, ip, request, response ) {
-  console.log("[Server] Client "+ip+" sent "+request.method+" request with url "+url);
+  console.log("[Server] Client " + ip + " sent " + request.method + " request with url " + url);
   let responsecontent = "";
   let ErrState = 0;
 
@@ -73,13 +38,7 @@ function RequestExcute( url, ip, request, response ) {
   } else if (url.includes("lua") || url.includes("cfg") || url.includes("ERROR_") ) {
     response.sendStatus(406);
   } else {
-    if (urlz.parse(url).query !== null && urlz.parse(url).pathname == "/icons") {
-      if (fs.existsSync('./icons/' + urlz.parse(url).query.substring(3))) {
-        fs.createReadStream('./icons/' + urlz.parse(url).query.substring(3)).pipe(response);
-      } else {
-        response.sendStatus(404);
-      } 
-    } else {
+    if (urlz.parse(url).query !== null) {
       dir = scandir()
       if ((dir) && (fs.existsSync(dir + url))) {
         var readFile = fs.readFile(dir + url, (err, data) => {
